70863c9e90c4521448371cf5d699444ceeef8bc4
Georges Bueno <georges.bueno@hotmail.com>
2022-06-10 00:58:27 -0400

    starting commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import {React, Component} from 'react';
+import './App.css';
+import Card from './card/card';
+
+class App extends Component {
+ 
+  render(){
+    return (
+      <div className="App">
+        <Card/>
+      </div>
+    );
+  }  
+}
+export default App;
---
+++ src/card/card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card =()=>{
+    return(
+        <div className="cont_card tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="placehodler" src="https://robohash.org/test" />
+            <div>
+                <h1>Hola nombre</h1>
+                <p>Correo</p>
+            </div>
+        </div>
+
+    );
+
+}
+export default Card;
