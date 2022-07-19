ec055ecaab3991b61bc42a86f5fcd322e92ebc81
Georges Bueno <georges.bueno@hotmail.com>
2022-07-18 20:24:49 -0400

    addind hooks
    added useState and useEffect hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,63 @@
 
-import {React, Component} from 'react';
+import {React, useEffect, useState} from 'react';
 import './App.css';
 import CardList from '../Components/card/CardList';
 import Scroll from '../Components/scroll/Scroll';
 import Search from '../Components/Search/search';
 
-class App extends Component {
-  constructor(){
+
+  
+function App () {
+ /*  constructor(){
     super()
     this.state = { //lo que puede cambiar y pasar a los componentes
       robots: [],
       searchfield: ''
     }
-  }
-  componentDidMount(){
+  } */
+
+  /* cambiamos los State a Hooks */
+  const [robots, setRobots] = useState([]);
+  const [searchfield,setSearchfield] = useState("");
+
+
+  
+  /* componentDidMount(){
 
     fetch('https://jsonplaceholder.typicode.com/users')//hacemos el request del api
   .then(response => response.json())
   .then(users => this.setState({robots: users}))//asignamos a robots el objeto para crear los cards
 
+  } */
+
+    /* CAMBIAMO EL COMPONENTDIDMOUNT POR UN USEEFFECT() */
+
+  useEffect( () => {
+     fetch('https://jsonplaceholder.typicode.com/users')//hacemos el request del api
+  .then(response => response.json())
+  .then(users => setRobots(users))//asignamos a robots el objeto para crear los cards
+
+  },[])
+  
+
+  const onSearchChange = (event)=>{
+    /* this.setState({searchfield: event.target.value}) */
+    setSearchfield(event.target.value)
   }
 
-  onSearchChange = (event)=>{
-    this.setState({searchfield: event.target.value})
-  }
+  
+    const Filtro = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase()); //verificamos el array si incluye el valor del campo
+    });
 
-  render(){
-    const Filtro = this.state.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()); //verificamos el array si incluye el valor del campo
-    })
-
-    if(this.state.robots.length === 0){
+    if(robots.length === 0){
       return <h1>Cargando...</h1>
     }else{
       return (
         <div className="App">
          <div className='header'> 
             <h1 className="f1" >CARD SEARCH</h1>
-            <Search searchChange = {this.onSearchChange}/>
+            <Search searchChange = {onSearchChange}/>
           </div>
           <Scroll>
             <CardList robots = {Filtro}/>
@@ -47,6 +67,5 @@ class App extends Component {
        
       );
     }
-  }  
 }
 export default App;
